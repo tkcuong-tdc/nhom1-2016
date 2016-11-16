f6e2c0aa4ce3d65cc6e900d7cbe9f4ed6de9a556
tkcuong-tdc <tkcuong.fee.hut@gmail.com>
2016-11-16 11:25:16 +0700

    Khoi tao Project by Tieu Kim Cuong, ngay 16-11-2016

---
+++ Nhom1/gen/vn/edu/tdc/nhom1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package vn.edu.tdc.nhom1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Nhom1/src/vn/edu/tdc/nhom1/MainLayoutActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.tdc.nhom1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainLayoutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
